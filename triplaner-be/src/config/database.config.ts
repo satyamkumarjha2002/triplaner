90bfb4c91cfbad5832c2f061f5ae0d2aa323e6ca
Satyam Kumar Jha <satyambhr2@gmail.com>
2025-05-05 01:32:11 +0530

    DB configuration

--- triplaner-be/src/config/database.config.ts
+++ triplaner-be/src/config/database.config.ts
@@ -1,4 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
-}; 
+  type: 'postgres',
+  url: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true, // Set to false in production
+  // Only log errors, not SQL queries
+  logging: ["error"],
+  retryAttempts: 5,
+  retryDelay: 3000,
+};
